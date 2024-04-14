52a0c204a3766ea231167d79ab6b0c7ad88c044c
Dusan Jovanov <jovanovdusan1@gmail.com>
2024-04-14 11:28:54 +0200

    fix history

---
+++ src/router/history.ts
@@ -0,0 +1,1 @@
+export { createBrowserHistory, createMemoryHistory } from "history";
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,4 +2,5 @@ export * from "./Link";
 export * from "./Route";
 export * from "./Router";
 export * from "./RouterProvider";
+export * from "./history";
 export * from "./types";
